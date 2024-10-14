f55bd7a77d1e6a6219fd3e1a254b3a0febef68a2
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-10-14 19:34:48 -0300

    first commit

---
+++ src/main/java/dev/Java10x/CadastroMaterias/CadastroMateriasApplication.java
@@ -0,0 +1,13 @@
+package dev.Java10x.CadastroMaterias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroMateriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroMateriasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Java10x/CadastroMaterias/CadastroMateriasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Java10x.CadastroMaterias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroMateriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
