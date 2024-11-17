e1d356de949f9736ee38519de5e8d91fbf2280fd
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-11-16 21:04:20 -0300

    feat: inscricao controller

--- src/main/java/dev/Java10x/CadastroMaterias/Inscricao/InscricaoController.java
+++ src/main/java/dev/Java10x/CadastroMaterias/Inscricao/InscricaoController.java
@@ -1,8 +1,10 @@
 package dev.Java10x.CadastroMaterias.Inscricao;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -10,14 +12,48 @@ import java.util.List;
 @RequestMapping("/inscricao")
 public class InscricaoController {
 
-    private InscricaoService inscricaoService;
+    private final InscricaoService inscricaoService;
 
     public InscricaoController(InscricaoService inscricaoService){
         this.inscricaoService = inscricaoService;
     }
 
     @GetMapping("/listarTodos")
-    public List<InscricaoModel> listarTodos(){
-        return inscricaoService.listarTodos();
+    public ResponseEntity<List<?>> listarTodos(){
+        List<InscricaoDTO> inscricoes = inscricaoService.listarTodos();
+        return ResponseEntity.ok().body(inscricoes);
+    }
+
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<?> listarPorId(@PathVariable Long id){
+        InscricaoDTO inscricao = inscricaoService.listarPorId(id);
+        if(inscricao!=null){
+            return ResponseEntity.ok().body(inscricao);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<InscricaoDTO> insertInscricao(@RequestBody InscricaoDTO inscricaoDTO){
+        InscricaoDTO newInscricao = inscricaoService.insertInscricao(inscricaoDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newInscricao);
+    }
+
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<?> alterarporId(@PathVariable Long id, @RequestBody InscricaoDTO inscricao){
+        if(inscricaoService.listarPorId(id)!=null){
+            InscricaoDTO newInscricao = inscricaoService.atualizarInscricao(id, inscricao);
+            return ResponseEntity.ok().body(newInscricao);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Inscrição não encontrada!");
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletarPorId(@PathVariable Long id){
+        if(inscricaoService.listarPorId(id)!=null){
+            inscricaoService.deletarPorid(id);
+            ResponseEntity.ok().body("Deletado com sucesso!");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Inscrição não encontrada!");
     }
 }
--- src/main/java/dev/Java10x/CadastroMaterias/Inscricao/InscricaoService.java
+++ src/main/java/dev/Java10x/CadastroMaterias/Inscricao/InscricaoService.java
@@ -2,17 +2,49 @@ package dev.Java10x.CadastroMaterias.Inscricao;
 
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class InscricaoService {
-    private InscricaoRepository inscricaoRepository;
 
-    public InscricaoService(InscricaoRepository inscricaoRepository){
+    private InscricaoRepository inscricaoRepository;
+    private InscricaoMapper inscricaoMapper;
+
+    public InscricaoService(InscricaoRepository inscricaoRepository, InscricaoMapper inscricaoMapper){
         this.inscricaoRepository =  inscricaoRepository;
+        this.inscricaoMapper = inscricaoMapper;
     }
 
-    public List<InscricaoModel> listarTodos(){
-        return inscricaoRepository.findAll();
+    public List<InscricaoDTO> listarTodos(){
+        List<InscricaoModel> inscricao = inscricaoRepository.findAll();
+        return inscricao.stream().map(inscricaoMapper::map).collect(Collectors.toList());
+    }
+
+    public InscricaoDTO listarPorId(Long id){
+        Optional<InscricaoModel> inscricao = inscricaoRepository.findById(id);
+        return inscricao.map(inscricaoMapper::map).orElse(null);
+    }
+
+    public InscricaoDTO insertInscricao(InscricaoDTO inscricaoDTO){
+        InscricaoModel inscricaoModel = inscricaoMapper.map(inscricaoDTO);
+        inscricaoModel = inscricaoRepository.save(inscricaoModel);
+        return inscricaoMapper.map(inscricaoModel);
+    }
+
+    public void deletarPorid(Long id){
+        inscricaoRepository.deleteById(id);
+    }
+
+    public InscricaoDTO atualizarInscricao(Long id, InscricaoDTO inscricao){
+        Optional<InscricaoModel> inscricaoExists = inscricaoRepository.findById(id);
+        if(inscricaoExists.isPresent()){
+            InscricaoModel newInscricao = inscricaoMapper.map(inscricao);
+            newInscricao.setId(id);
+            InscricaoModel inscricaoSalva = inscricaoRepository.save(newInscricao);
+            return inscricaoMapper.map(inscricaoSalva);
+        }
+        return null;
     }
 
 }
